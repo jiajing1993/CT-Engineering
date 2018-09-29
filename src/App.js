ff232d979f5b64e25c6d0b6d2a691b8a8d535fcb
Loh Jia Jing <richmanxinbox@gmail.com>
2018-09-29 23:13:01 +0800

    inst - (scss) - install sass-node

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { Route } from 'react-router-dom';
 import Header from './components/Header'
 import Home from './pages/Home'
