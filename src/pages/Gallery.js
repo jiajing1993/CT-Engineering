a3f29af6ac7109431599cb15f1b802529d62ae5a
Loh Jia Jing <richmanxinbox@gmail.com>
2018-10-23 17:39:57 +0800

    upload gallery images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,14 +8,113 @@ import jumbotronImage from '../images/company-jumbotron.png'
 
 import '../styles/gallery.scss'
 
+import image1 from '../images/gallery/image1.jpg'
+import image2 from '../images/gallery/image2.jpg'
+import image3 from '../images/gallery/image3.JPG'
+import image4 from '../images/gallery/image4.JPG'
+import image5 from '../images/gallery/image5.JPG'
+import image6 from '../images/gallery/image6.JPG'
+import image7 from '../images/gallery/image7.JPG'
+import image8 from '../images/gallery/image8.JPG'
+import image9 from '../images/gallery/image9.JPG'
+import image10 from '../images/gallery/image10.JPG'
+import image11 from '../images/gallery/image11.JPG'
+import image12 from '../images/gallery/image12.JPG'
+import image13 from '../images/gallery/image13.JPG'
 
 
-const IMAGES =[{
-  src: "https://4b15vr13762d1t2kqp47j8d4-wpengine.netdna-ssl.com/wp-content/uploads/construction-site-2858310_1920.jpg",
-  thumbnail: "{https://4b15vr13762d1t2kqp47j8d4-wpengine.netdna-ssl.com/wp-content/uploads/construction-site-2858310_1920.jpg",
-  thumbnailWidth: 500,
-  thumbnailHeight: 300
-}
+
+const IMAGES =[
+  {
+    src: `${image1}`,
+    thumbnail: `${image1}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image2}`,
+    thumbnail: `${image2}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image3}`,
+    thumbnail: `${image3}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image4}`,
+    thumbnail: `${image4}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image5}`,
+    thumbnail: `${image5}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image6}`,
+    thumbnail: `${image6}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image7}`,
+    thumbnail: `${image7}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image8}`,
+    thumbnail: `${image8}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image9}`,
+    thumbnail: `${image9}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image10}`,
+    thumbnail: `${image10}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image11}`,
+    thumbnail: `${image11}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image12}`,
+    thumbnail: `${image12}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image13}`,
+    thumbnail: `${image13}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image4}`,
+    thumbnail: `${image4}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
+  {
+    src: `${image5}`,
+    thumbnail: `${image5}`,
+    thumbnailWidth: 500,
+    thumbnailHeight: 300
+  },
 ]
 
 export default class Gallery extends Component {
