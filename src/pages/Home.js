80c8cd51156d1ed6957143e85da92efdf9f44492
Loh Jia Jing <richmanxinbox@gmail.com>
2018-10-23 16:51:58 +0800

    fix (gallery/img) - deleted all gallery images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ export default class Home extends Component {
         <div className="text-box">
           <p className="title">The True Passion of vigor. </p>
           <p className="description">We will build rapport, trust and the foundation in which our reputation will rest rock solid and assure into the future.</p>
-          <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='4lN4Vg4fdpo' youtube={{autohide: 1, controls: 0, showinfo: 0, rel:0}} onClose={() => this.setState({isOpen: false})} />
+          <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='4lN4Vg4fdpo' youtube={{showinfo: 0, rel:0}} onClose={() => this.setState({isOpen: false})} />
           <button onClick={this.openModal}>Play Video</button>
         </div>
       </div>
