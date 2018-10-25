2588075e26ed143d7724510c581d7df7caedf26a
Loh Jia Jing <richmanxinbox@gmail.com>
2018-10-25 11:34:59 +0800

    chore (ScrollToTopOnMount) create a component to force go top whenever it mount

---
+++ src/components/ScrollToTopOnMount.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ScrollToTopOnMount extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+
