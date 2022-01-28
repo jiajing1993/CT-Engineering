ba0bf3172d7c52c52bc8146f527ac6c493a3e8b7
Jia Jing <jiajing19932@gmail.com>
2022-01-29 01:16:47 +0800

    update main video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ export default class Home extends Component {
         <div className="text-box">
           <p className="title">The True Passion of vigor. </p>
           <p className="description">We will build rapport, trust and the foundation in which our reputation will rest rock solid and assure into the future.</p>
-          <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='4lN4Vg4fdpo' youtube={{showinfo: 0, rel:0}} onClose={() => this.setState({isOpen: false})} />
+          <ModalVideo channel='vimeo' isOpen={this.state.isOpen} videoId='671227203' youtube={{showinfo: 0, rel:0}} onClose={() => this.setState({isOpen: false})} />
           <button onClick={this.openModal}>Play Video</button>
         </div>
       </div>
